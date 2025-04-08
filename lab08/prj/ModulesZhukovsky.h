f2e45d1cf260e25d540a976277050830551be125
Siin0i <vladkazhu@gmail.com>
2025-04-08 18:29:14 +0300

    Added .h fail

---
+++ lab08/prj/ModulesZhukovsky.h
@@ -0,0 +1,13 @@
+#ifndef MODULESZHUKOVSKY_H
+#define MODULESZHUKOVSKY_H
+
+double s_calculation(double x, double y, double z);
+void developer_info();
+
+//lab 9
+#include <string>
+char calculateRating(int test1, int test2, int exam);
+std::string convertShoeSize(double size_cm);
+int binaryBitCount(unsigned int N);
+
+#endif // MODULESZHUKOVSKY_H
