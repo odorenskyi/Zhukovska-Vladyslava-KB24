f1ac46a38e1a0375f37c2f802d8dc90b9f62cd90
Siin0i <vladkazhu@gmail.com>
2025-04-14 18:21:03 +0300

    Added new file .h

--- lab08/prj/ModulesZhukovsky.h
+++ lab08/prj/ModulesZhukovsky.h
@@ -10,4 +10,9 @@ char calculateRating(int test1, int test2, int exam);
 std::string convertShoeSize(double size_cm);
 int binaryBitCount(unsigned int N);
 
+//lab 10
+void task10_1(const std::string& inputFilename, const std::string& outputFilename);
+void task10_2(const std::string& outputFilename);
+void task10_3(double x, double y, double z, unsigned int b);
+
 #endif // MODULESZHUKOVSKY_H
